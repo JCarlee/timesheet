6fe3d728d42366d878897d63941f950a8d48d247
jcarlee <jcarlee@gmail.com>
2018-07-13 17:16:06 -0500

    Moved to oop

--- timesheet.py
+++ timesheet.py
@@ -2,33 +2,45 @@ import shutil
 import datetime
 import os
 
-root = 'G:\My Drive\Timesheets\\'  # Root dir only usable on John Carlee's work desktop
-today = datetime.datetime.now()    # Set today to current date
 
-day1 = today - datetime.timedelta(days=6)          # Define first day of excel title
-day1_str = '{0}.{1}'.format(day1.month, day1.day)  # Create usable string for day1
+class Timesheet(object):
+    def __init__(self):
+        self.root = 'G:\My Drive\Timesheets\\'  # Root dir only usable on John Carlee's work desktop
+        self.today = datetime.datetime.now()    # Set today to current date
 
-day2 = day1 + datetime.timedelta(days=6)           # Define second day of excel title
-day2_str = '{0}.{1}'.format(day2.month, day2.day)  # Create usable string for day2
+        self.day1 = self.today - datetime.timedelta(days=6)          # Define first day of excel title
+        self.day1_str = '{0}.{1}'.format(self.day1.month, self.day1.day)  # Create usable string for day1
 
-year = '{0}'.format(day1.year)                     # Variable to hold year string
+        self.day2 = self.day1 + datetime.timedelta(days=6)           # Define second day of excel title
+        self.day2_str = '{0}.{1}'.format(self.day2.month, self.day2.day)  # Create usable string for day2
 
-xlsx_list = []                                     # Empty list holding xlsx files in root
-pdf_list = []                                      # Empty list holding pdf files in root
+        self.year = '{0}'.format(self.day1.year)                     # Variable to hold year string
 
-for file in os.listdir(root):                      # Loop through root, make list of xlsx files
-    if file.endswith(".xlsx"):
-        xlsx_list.append(file)
+        self.xlsx_list = []                                     # Empty list holding xlsx files in root
+        self.pdf_list = []                                      # Empty list holding pdf files in root
+        self.new_xlsx = \
+            self.root + 'Carlee_timesheet_' + self.day1_str + '-' + self.day2_str + '_' + self.year + '.xlsx'
 
-for file in os.listdir(root):                      # Loop through root, make list of pdf files
-    if file.endswith(".pdf"):
-        pdf_list.append(file)
+    def dir_list(self):
+        for file in os.listdir(self.root):                      # Loop through root, make list of xlsx files
+            if file.endswith(".xlsx"):
+                self.xlsx_list.append(file)
 
-# define new xlsx path
-new_xlsx = root + 'Carlee_timesheet_' + day1_str + '-' + day2_str + '_' + year + '.xlsx'
+        for file in os.listdir(self.root):                      # Loop through root, make list of pdf files
+            if file.endswith(".pdf"):
+                self.pdf_list.append(file)
 
-shutil.copy(root + xlsx_list[0], new_xlsx)         # Make copy of previous week's timesheet with next week title
+    def create_new(self):
+        # Make copy of previous week's timesheet with next week title
+        shutil.copy(self.root + self.xlsx_list[0], self.new_xlsx)
 
-# Move both old files into old folder
-shutil.move(root + xlsx_list[0], root + 'old\\' + xlsx_list[0])
-shutil.move(root + pdf_list[0], root + 'old\\' + pdf_list[0])
+    def archive(self):
+        # Move both old files into old folder
+        shutil.move(self.root + self.xlsx_list[0], self.root + 'old\\' + self.xlsx_list[0])
+        shutil.move(self.root + self.pdf_list[0], self.root + 'old\\' + self.pdf_list[0])
+
+
+a = Timesheet()
+a.dir_list()
+a.create_new()
+a.archive()
