c1eb768a9b01bb310b53d06bd1405d5fe1f02a8c
John Carlee <jcarlee@gmail.com>
2018-07-16 13:44:59 -0500

    commenting

--- timesheet.py
+++ timesheet.py
@@ -5,28 +5,28 @@ import os
 
 class Timesheet(object):
     def __init__(self):
-        self.root = 'G:\My Drive\Timesheets\\'  # Root dir only usable on John Carlee's work desktop
-        self.today = datetime.datetime.now()    # Set today to current date
+        self.root = 'G:\My Drive\Timesheets\\'  # Root dir only usable on John Carlee's work desktop on Fridays
+        self.today = datetime.datetime.now()    # Set 'today' to current date
 
-        self.day1 = self.today - datetime.timedelta(days=6)          # Define first day of excel title
+        self.day1 = self.today - datetime.timedelta(days=6)               # Define first day of excel title
         self.day1_str = '{0}.{1}'.format(self.day1.month, self.day1.day)  # Create usable string for day1
 
-        self.day2 = self.day1 + datetime.timedelta(days=6)           # Define second day of excel title
+        self.day2 = self.day1 + datetime.timedelta(days=6)                # Define second day of excel title
         self.day2_str = '{0}.{1}'.format(self.day2.month, self.day2.day)  # Create usable string for day2
 
-        self.year = '{0}'.format(self.day1.year)                     # Variable to hold year string
+        self.year = '{0}'.format(self.day1.year)                          # Variable to hold year string
 
-        self.xlsx_list = []                                     # Empty list holding xlsx files in root
-        self.pdf_list = []                                      # Empty list holding pdf files in root
+        self.xlsx_list = []                                               # Empty list holding xlsx files in root
+        self.pdf_list = []                                                # Empty list holding pdf files in root
         self.new_xlsx = \
             self.root + 'Carlee_timesheet_' + self.day1_str + '-' + self.day2_str + '_' + self.year + '.xlsx'
 
     def dir_list(self):
-        for file in os.listdir(self.root):                      # Loop through root, make list of xlsx files
+        for file in os.listdir(self.root):                                # Loop through root, make list of xlsx files
             if file.endswith(".xlsx"):
                 self.xlsx_list.append(file)
 
-        for file in os.listdir(self.root):                      # Loop through root, make list of pdf files
+        for file in os.listdir(self.root):                                # Loop through root, make list of pdf files
             if file.endswith(".pdf"):
                 self.pdf_list.append(file)
 
